7ffc86b408ecb9f4fe60c74196ea9251090d6dfa
Corentin Chateau <Corentin.Chateau.49@gmail.com>
2016-12-15 10:25:23 +0100

    Tasks

--- js/plumo-tasks/services/tasksService.js
+++ js/plumo-tasks/services/tasksService.js
@@ -1,5 +1,5 @@
 (function (angular) {
-    angular.module('plumo-task').factory('TasksService',Factory);
+    angular.module('plumo-tasks').factory('TasksService',Factory);
     Factory.$inject = ['$resource'];
     function Factory($resource) {
         return $resource('http://localhost:8000/task/:id',{id :'@id'});

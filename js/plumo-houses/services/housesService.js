ed35e0d39601644491614e9bc832a071f98305bd
lucien-le-foll <l.lefoll@soqrate.com>
2017-02-06 11:33:35 +0100

    Modifying the houseManager

--- js/plumo-houses/services/housesService.js
+++ js/plumo-houses/services/housesService.js
@@ -5,27 +5,17 @@
     Factory.$inject = ['HTTPClient'];
     function Factory(HTTPClient) {
         return {
-            getCurrentHouse: function () {
-                return HTTPClient.get('/user/house').then(function (response) {
-                    return response.data;
-                    console.log(data);
-                }, function (response) {
-                    return response;
-                });
+            getHouse: function () {
+                return HTTPClient.get('/house');
             },
             saveHouse: function (house) {
-                // TODO intégrer l'ajout de pièces à la création
                 var httpObject = angular.copy(house);
                 delete httpObject.id;
                 if (house.id!==null) {
-                    return HTTPClient.put('/house/' + house.id, httpObject).then(function (response) {
-                        return response.data;
-                    });
+                    return HTTPClient.put('/house/' + house.id, httpObject);
                 }
                 else {
-                    return HTTPClient.post('/house', httpObject).then(function (response) {
-                        return response.data;
-                    });
+                    return HTTPClient.post('/house', httpObject);
                 }
             }
         };
